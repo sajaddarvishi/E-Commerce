5080f766588ed5ebddef6e82a7f7106dc49f61e2
sajaddarvishi <darvishi.tm@gmail.com>
2024-09-05 21:45:15 +0330

    add django-rest-framework

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .products import products
 
 
 # Create your views here.
 
-
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         '/api/products/',
@@ -21,7 +24,19 @@ def getRoutes(request):
         '/api/products/delete/<id>/',
         '/api/products/<update>/<id>/',
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
 
+@api_view(['GET'])
 def getProducts(request):
-    return JsonResponse (products, safe=False)
+    return Response(products)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+        
+    return Response(product)
