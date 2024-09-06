3cbe01b9845748b3f85ad84e6f0a3b0c5ca25e18
sajaddarvishi <darvishi.tm@gmail.com>
2024-09-06 07:52:50 +0330

    add cart functionality

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -8,7 +8,7 @@ import {
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAIL,
 
- } from '../constants/productConstans'
+ } from '../constants/productConstants'
 
  export const listProducts = () => async(dispatch) => {
     try {
