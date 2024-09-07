dba649cdab2a49f3c31e12220e92904eb2d2dbab
sajaddarvishi <darvishi.tm@gmail.com>
2024-09-07 22:57:17 +0330

    add user screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+
 
 
 
@@ -18,6 +20,7 @@ function App() {
         <Container>
           <Routes>
             <Route path='/' Component={HomeScreen} exact/>
+            <Route path='/login' Component={LoginScreen} />
             <Route path='/product/:id' Component={ProductScreen}/>
             <Route path='/cart/:id?' Component={CartScreen} />
           </Routes>
