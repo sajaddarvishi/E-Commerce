6f647a46a0d315557b8cbee037f11e02d25d453f
sajaddarvishi <darvishi.tm@gmail.com>
2024-09-09 22:40:31 +0330

    add reducer&action for order details

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -5,6 +5,10 @@ import {
 
     ORDER_CREATE_RESET,
 
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_FAIL,
+
    
 } from '../constants/orderConstants'
 
@@ -36,3 +40,28 @@ export const orderCreateReducer = (state = {}, action) => {
     }
 }
 
+export const orderDetailsReducer = (state = { loading: true, orderItems: [], shippingAddress: {} }, action) => {
+    switch (action.type) {
+        case ORDER_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+
+        case ORDER_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                order: action.payload
+            }
+
+        case ORDER_DETAILS_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+
+        default:
+            return state
+    }
+}
