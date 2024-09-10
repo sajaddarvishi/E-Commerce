7e8c6224d866af648c0dd41abd1874b305d023fc
sajaddarvishi <darvishi.tm@gmail.com>
2024-09-10 23:09:19 +0330

    add orders in profile

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -5,7 +5,9 @@ from base.views import order_views as views
 urlpatterns = [
 
     path('add/', views.addOrderItems, name='orders-add'),
+    path('myorders/', views.getMyOrders, name='myorders'),
+
     path('<str:pk>/', views.getOrderById, name='user-order'),
-        path('<str:pk>/pay/', views.getOrderById, name='pay'),
+    path('<str:pk>/pay/', views.getOrderById, name='pay'),
 
 ]
