2e9a8d6540c261b042e045edd3d82da486dfcab4
sajaddarvishi <darvishi.tm@gmail.com>
2024-09-11 07:53:47 +0330

    admin delete product

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -20,3 +20,10 @@ def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response('Producted Deleted')
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { legacy_createStore as createStore , combineReducers, applyMiddleware} from 'redux';
 import {thunk} from 'redux-thunk';
 import { composeWithDevTools } from '@redux-devtools/extension';
-import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import { productListReducer, productDetailsReducer, productDeleteReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers'
 import { 
     userLoginReducer, 
@@ -18,7 +18,10 @@ import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyRe
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    productDelete: productDeleteReducer,
+
     cart: cartReducer,
+    
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
